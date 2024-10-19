78ae5766088854c7f57928f52e94ce26a92830d8
baruaeee <s.b_04@yahoo.com>
2024-10-19 18:01:03 +0200

    auto_sized_grid added

---
+++ Fabric/tasks/synthesizable_verilog/run_auto/k6_frac_N10_tileable_40nm/and2/MIN_ROUTE_CHAN_WIDTH/SRC/fpga_defines.v
@@ -0,0 +1,9 @@
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Preprocessing flags to enable/disable features in FPGA Verilog modules
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Sat Oct 19 17:55:19 2024
+//-------------------------------------------
+`define ENABLE_TIMING 1
+
---
+++ Fabric/tasks/synthesizable_verilog/run_auto/k6_frac_N10_tileable_40nm/and2/MIN_ROUTE_CHAN_WIDTH/SRC/sub_module/arch_encoder.v
@@ -0,0 +1,7 @@
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Decoders for fabric configuration protocol
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Sat Oct 19 17:55:19 2024
+//-------------------------------------------
