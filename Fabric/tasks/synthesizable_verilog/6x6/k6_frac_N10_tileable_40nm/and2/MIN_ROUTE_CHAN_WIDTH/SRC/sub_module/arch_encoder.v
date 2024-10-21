5b0fea87c44cc0b7d05b781efef9e51a2ef5b7e3
baruaeee <s.b_04@yahoo.com>
2024-10-21 18:16:01 +0200

    6x6 update

--- Fabric/tasks/synthesizable_verilog/6x6/k6_frac_N10_tileable_40nm/and2/MIN_ROUTE_CHAN_WIDTH/SRC/sub_module/arch_encoder.v
+++
@@ -1,7 +0,0 @@
-//-------------------------------------------
-//	FPGA Synthesizable Verilog Netlist
-//	Description: Decoders for fabric configuration protocol
-//	Author: Xifan TANG
-//	Organization: University of Utah
-//	Date: Sun Oct 20 15:06:19 2024
-//-------------------------------------------
--- Fabric/tasks/synthesizable_verilog/6x6/k6_frac_N10_tileable_40nm/and2/MIN_ROUTE_CHAN_WIDTH/SRC/sub_module/local_encoder.v
+++
@@ -1,7 +0,0 @@
-//-------------------------------------------
-//	FPGA Synthesizable Verilog Netlist
-//	Description: Local Decoders for Multiplexers
-//	Author: Xifan TANG
-//	Organization: University of Utah
-//	Date: Sun Oct 20 15:06:19 2024
-//-------------------------------------------
