b24415efb0d978105abe34023e18d947a6c940f5
baruaeee <s.b_04@yahoo.com>
2024-10-21 17:53:35 +0200

    7x7 fabric

---
+++ Fabric/tasks/synthesizable_verilog/7x7/SRC/fpga_defines.v
@@ -0,0 +1,9 @@
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Preprocessing flags to enable/disable features in FPGA Verilog modules
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Mon Oct 21 17:40:44 2024
+//-------------------------------------------
+`define ENABLE_TIMING 1
+
