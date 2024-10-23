ed28b54e2511d4591b6db2a7332544440b136381
baruaeee <s.b_04@yahoo.com>
2024-10-23 21:26:32 +0200

    sky130_dfrbp_1 trial as DFFR replacement

---
+++ Fabric/tasks/synthesizable_verilog/run002/k6_frac_N10_tileable_40nm/and2/MIN_ROUTE_CHAN_WIDTH/SRC/sub_module/mux_primitives.v
@@ -0,0 +1,7 @@
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Multiplexer primitives
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Wed Oct 23 21:24:52 2024
+//-------------------------------------------
