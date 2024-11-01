b2377dea30f8f0185c0bf7bd97bcb24ecf6a214a
baruaeee <s.b_04@yahoo.com>
2024-11-01 18:15:17 +0100

    3x3 added

---
+++ Fabric/tasks/synthesizable_verilog/auto_3x3/k6_frac_N10_tileable_40nm/and2/MIN_ROUTE_CHAN_WIDTH/SRC/sub_module/arch_encoder.v
@@ -0,0 +1,7 @@
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Decoders for fabric configuration protocol
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Fri Nov  1 18:05:25 2024
+//-------------------------------------------
---
+++ Fabric/tasks/synthesizable_verilog/auto_3x3/k6_frac_N10_tileable_40nm/and2/MIN_ROUTE_CHAN_WIDTH/SRC/sub_module/local_encoder.v
@@ -0,0 +1,7 @@
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Local Decoders for Multiplexers
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Fri Nov  1 18:05:25 2024
+//-------------------------------------------
