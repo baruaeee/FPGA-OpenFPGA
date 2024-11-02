7f876496d05a5f0f933bfd7d92b405a9ed4ed5f0
Sajib Barua (Student MA about OpenFPGA) <cae1@cae-europractice1.othr.de>
2024-11-02 21:48:06 +0100

    pnr 3x3

---
+++ PNR/auto_3x3/SRC/sub_module/local_encoder.v
@@ -0,0 +1,7 @@
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Local Decoders for Multiplexers
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Sat Oct 26 04:14:15 2024
+//-------------------------------------------
