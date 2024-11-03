513673e5679a6a1d6218879345df63fb71a583ec
Sajib Barua (Student MA about OpenFPGA) <cae1@cae-europractice1.othr.de>
2024-11-03 01:30:13 +0100

    GPIO OSU

--- Synthesis/GPIO/fv/GPIO/read_libs.tcl
+++ Synthesis/GPIO/fv/GPIO/read_libs.tcl
@@ -1,4 +1,4 @@
 add_search_path . /eda/cadence/2023-24/RHELx86/DDIEXPORT_22.33.000/GENUS221/tools.lnx86/lib/tech -library -both
 read_library -liberty -both \
-    /home/cae1/Desktop/FPGA-OpenFPGA/Synthesis/GPIO/sky130_fd_sc_hd__tt_025C_1v80.lib
+    /home/cae1/Desktop/FPGA-OpenFPGA/Synthesis/GPIO/sky130_osu_sc_18T_hs_TT_1P8_25C.ccs.lib
 
