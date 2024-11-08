915cf65229e5b918571169ec9a8a4c945b04f79a
baruaeee <s.b_04@yahoo.com>
2024-11-08 14:25:39 +0100

    misc

--- PNR/SRC/fpga_defines.v
+++
@@ -1,9 +0,0 @@
-//-------------------------------------------
-//	FPGA Synthesizable Verilog Netlist
-//	Description: Preprocessing flags to enable/disable features in FPGA Verilog modules
-//	Author: Xifan TANG
-//	Organization: University of Utah
-//	Date: Sat Oct 26 04:14:15 2024
-//-------------------------------------------
-`define ENABLE_TIMING 1
-
--- PNR/SRC/sub_module/local_encoder.v
+++
@@ -1,7 +0,0 @@
-//-------------------------------------------
-//	FPGA Synthesizable Verilog Netlist
-//	Description: Local Decoders for Multiplexers
-//	Author: Xifan TANG
-//	Organization: University of Utah
-//	Date: Sat Oct 26 04:14:15 2024
-//-------------------------------------------
