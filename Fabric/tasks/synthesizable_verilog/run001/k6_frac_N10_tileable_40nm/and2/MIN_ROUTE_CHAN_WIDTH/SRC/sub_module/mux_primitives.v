53a5374d058e34c5ddf96dcdc2c18c1336c31b40
baruaeee <s.b_04@yahoo.com>
2024-11-08 18:44:40 +0100

    misc

--- Fabric/tasks/synthesizable_verilog/run001/k6_frac_N10_tileable_40nm/and2/MIN_ROUTE_CHAN_WIDTH/SRC/sub_module/mux_primitives.v
+++
@@ -1,7 +0,0 @@
-//-------------------------------------------
-//	FPGA Synthesizable Verilog Netlist
-//	Description: Multiplexer primitives
-//	Author: Xifan TANG
-//	Organization: University of Utah
-//	Date: Fri Nov  8 15:44:44 2024
-//-------------------------------------------
--- Fabric/tasks/synthesizable_verilog/run001/k6_frac_N10_tileable_40nm/and2/MIN_ROUTE_CHAN_WIDTH/SRC/sub_module/shift_register_banks.v
+++
@@ -1,7 +0,0 @@
-//-------------------------------------------
-//	FPGA Synthesizable Verilog Netlist
-//	Description: Shift register banks used in FPGA
-//	Author: Xifan TANG
-//	Organization: University of Utah
-//	Date: Fri Nov  8 15:44:44 2024
-//-------------------------------------------
