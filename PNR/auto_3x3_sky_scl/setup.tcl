b2eb1df2b88e14f5eed87e26cfb49460b321287e
Sajib Barua (Student MA about OpenFPGA) <cae1@cae-europractice1.othr.de>
2024-11-21 02:23:06 +0100

    misc

--- PNR/auto_3x3_sky_scl/setup.tcl
+++ PNR/auto_3x3_sky_scl/setup.tcl
@@ -1,10 +1,11 @@
 ## Specify floorplan
-floorPlan -coreMarginsBy io -r 1.00 0.71 8 8 8 8
+#floorPlan -coreMarginsBy io -r 1.00 0.71 8 8 8 8
 #floorPlan -site 18T -b 0 0 1468.665 1468.665 199.856 200.098 1265 1265 220.016 220.078 1245 1245
+floorPlan -site CoreSite -noSnapToGrid -d 2118.665 2118.665 80 80 80 80
 #floorPlan -site 18T -noSnapToGrid -d 1468.665 1468.665 80 80 80 80
 # 0 0 1468.665 1468.665 199.856 200.098 1265 1265 220.016 220.078 1245 1245
 
-#loadIoFile IO_PAD.io
+loadIoFile IO_PAD.io
 
 ## IO filler
 # no IO filler cell in sky 130
@@ -12,26 +13,40 @@ floorPlan -coreMarginsBy io -r 1.00 0.71 8 8 8 8
 setDesignMode -process 130
 
 ## P/G connect
-globalNetConnect VDD -type pgpin -pin VDD -override -verbose -netlistOverride
-globalNetConnect VSS -type pgpin -pin VSS -override -verbose -netlistOverride
-#globalNetConnect VSSIO -type pgpin -pin VSSIO -override -verbose -netlistOverride
-#globalNetConnect VDDIO -type pgpin -pin VDDIO -override -verbose -netlistOverride
+globalNetConnect VDD -type pgpin -pin vdd -override -verbose -netlistOverride
+globalNetConnect VSS -type pgpin -pin gnd -override -verbose -netlistOverride
+
+globalNetConnect VDD -type pgpin -pin VDDIO -override -verbose -netlistOverride
+globalNetConnect VSS -type pgpin -pin VSSIO -override -verbose -netlistOverride
+
+globalNetConnect VSS -type pgpin -pin VSSD -override -verbose -netlistOverride
+
+globalNetConnect VDD -type pgpin -pin VSWITCH -override -verbose -netlistOverride
+
+globalNetConnect VDD -type pgpin -pin VDDA -override -verbose -netlistOverride
+globalNetConnect VSS -type pgpin -pin VSSA -override -verbose -netlistOverride
+
+globalNetConnect VDD -type pgpin -pin VDDIO_Q -override -verbose -netlistOverride
+globalNetConnect VSS -type pgpin -pin VSSIO_Q -override -verbose -netlistOverride
+
+globalNetConnect VDD -type pgpin -pin P_CORE -override -verbose -netlistOverride
+globalNetConnect VSS -type pgpin -pin G_CORE -override -verbose -netlistOverride
 
 ## Add Power Ring
 addRing -nets {VDD VSS} \
 	-type core_rings \
 	-follow core \
 	-layer {top met5 bottom met5 left met4 right met4} \
-	-width {top 1.8 bottom 1.8 left 1.8 right 1.8} \
-	-spacing {top 1.8 bottom 1.8 left 1.8 right 1.8} \
-	-offset {top 1.8 bottom 1.8 left 1.8 right 1.8} \
+	-width {top 5 bottom 5 left 5 right 5} \
+	-spacing {top 5 bottom 5 left 5 right 5} \
+	-offset {top 20 bottom 20 left 20 right 20} \
 	-center 0 \
 	-threshold 0 \
 	-jog_distance 0 \
 	-snap_wire_center_to_grid None
 
 ## Add Special Route
-sroute -connect { blockPin padPin padRing corePin floatingStripe } \
+sroute -connect { blockPin padPin corePin floatingStripe } \
 	-layerChangeRange { met1(1) met5(5) } -blockPinTarget { nearestTarget } \
 	-padPinPortConnect { allPort oneGeom } -padPinTarget { nearestTarget } \
 	-corePinTarget { firstAfterRowEnd } \
@@ -47,10 +62,10 @@ sroute -connect { blockPin padPin padRing corePin floatingStripe } \
 addStripe -nets {VDD VSS} \
 	-layer met4 \
 	-direction vertical \
-	-width 2 -spacing 2 \
-	-set_to_set_distance 16 \
+	-width 5 -spacing 5 \
+	-set_to_set_distance 120 \
 	-start_from left \
-	-start_offset 10 \
+	-start_offset 120 \
 	-switch_layer_over_obs false \
 	-max_same_layer_jog_length 2 \
 	-padcore_ring_top_layer_limit met5 \
